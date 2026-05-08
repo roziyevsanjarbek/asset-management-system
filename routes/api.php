ab0225fe42c374feedd4a1efeb67d9fd6ba35bfd
Sanjarbek Roziyev <roziyevsanjarbekk@gmail.com>
2026-05-08 18:49:35 +0500

    add: employee CRUD and swagger documentation

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DeviceTypeController;
+use App\Http\Controllers\EmployeeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,4 +19,13 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/device-types/{id}', [DeviceTypeController::class, 'show']);
     Route::put('/device-types/{id}', [DeviceTypeController::class, 'update']);
     Route::delete('/device-types/{id}', [DeviceTypeController::class, 'destroy']);
+
+
+    Route::get('/employees', [EmployeeController::class, 'index']);
+    Route::post('/employees', [EmployeeController::class, 'store']);
+    Route::get('/employees/{id}', [EmployeeController::class, 'show']);
+    Route::put('/employees/{id}', [EmployeeController::class, 'update']);
+    Route::delete('/employees/{id}', [EmployeeController::class, 'destroy']);
+
+    
 });
